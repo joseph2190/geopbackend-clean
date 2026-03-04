6246cd8ba22594140d5d18f3b958e6ac1b7b34f4
dinelson <dinelsonwork@gmail.com>
2026-03-04 02:35:59 +0100

    dodo year fix

--- server.js
+++ server.js
@@ -319,15 +319,21 @@ app.post("/dodo-webhook", async (req, res) => {
       let tier = "free";
       let credits = 5;
 
-      if (productId === process.env.DODO_LITE_PRODUCT_ID) {
-        tier = "lite";
-        credits = 15;
-      }
+     if (
+  productId === process.env.DODO_LITE_MONTHLY_PRODUCT_ID ||
+  productId === process.env.DODO_LITE_YEARLY_PRODUCT_ID
+) {
+  tier = "lite";
+  credits = 15;
+}
 
-      if (productId === process.env.DODO_PRO_PRODUCT_ID) {
-        tier = "pro";
-        credits = 50;
-      }
+if (
+  productId === process.env.DODO_PRO_MONTHLY_PRODUCT_ID ||
+  productId === process.env.DODO_PRO_YEARLY_PRODUCT_ID
+) {
+  tier = "pro";
+  credits = 50;
+}
 
       await userRef.update({
         subscriptionTier: tier,
